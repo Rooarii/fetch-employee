7002d0d87fb77dac6342734bd542d35e8c6a9631
Rooarii <rooarii.manuel@gmail.com>
2020-10-22 05:01:10 -0400

    ajax request set up

--- src/App.js
+++ src/App.js
@@ -26,13 +26,45 @@ const sampleEmployee = {
   },
 };
 
-function App() {
-  return (
-    // call DisplayEmployee component
-    <>
-      <DisplayEmployee employee={sampleEmployee} /> {/*pass down sampleEmplyee as props*/}
-    </>
-  );
+// function App() {
+//   return (
+//     // call DisplayEmployee component
+//     <>
+//       <DisplayEmployee employee={sampleEmployee} /> {/*pass down sampleEmplyee as props*/}
+//     </>
+//   );
+// }
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employee: sampleEmployee
+    };
+    this.getEmployee = this.getEmployee.bind(this);
+  }
+  getEmployee() {
+    // Send the request
+    axios.get('https://randomuser.me/api?nat=fr')
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {
+        this.setState({
+          employee: data.results[0],
+        });
+    });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <DisplayEmployee employee={this.state.employee} />
+        <button type="button" onClick={this.getEmployee}>Get employee</button>
+      </div>
+    );
+  }
 }
 
 export default App;
